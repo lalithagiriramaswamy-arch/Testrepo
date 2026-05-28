fa04c5010d43f348937f7f62fc5f0bf2ddf96c7e
lalithagiriramaswamy-arch <lalitha.giriramaswamy@gmail.com>
2026-05-28 09:59:59 +0530

    Update grocery.py

--- grocery.py
+++ grocery.py
@@ -1,4 +1,5 @@
 item1 = input("Enter first grocery item: ")
 item2 = input("Enter second grocery item:")
 item3 = input("Enter third grocery item:")
-print(f"Your shopping list:{item1},{item2},{item3}")
+item4 = input("Enter fourth grocery item:")
+print(f"Your shopping list:{item1},{item2},{item3},{item4}")
